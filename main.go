d69e873430643fc6088dd1bef02dd9284386419a
Nitu Balwani <balwaninitu@gmail.com>
2021-06-18 16:34:06 +0800

    second commit

--- main.go
+++ main.go
@@ -2,33 +2,17 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	"go-web-app/pkg/handlers"
+
 	"net/http"
 )
 
 const portNumber = ":8080"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.html")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.html")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("cannot parsed templates", err)
-		return
-	}
-}
-
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Printf("Starting Application on port %s\n", portNumber)
 	http.ListenAndServe(portNumber, nil)
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"go-web-app/pkg/render"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.html")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.html")
+}
