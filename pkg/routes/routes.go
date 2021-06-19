2f54a23cd649d3f6ee9115fe2e844f316d00d57a
Nitu Balwani <balwaninitu@gmail.com>
2021-06-19 10:56:24 +0800

    chi router 11th commit

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -5,15 +5,17 @@ import (
 	"go-web-app/pkg/handlers"
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func Routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+	//recover from panic
+	mux.Use(middleware.Recoverer)
 
-	mux := pat.New()
-
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
